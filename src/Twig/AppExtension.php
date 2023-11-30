4f74a8d2462f439a747bce705b92892d53134833
jeremy Dubrulle <https://github.com/jeremy93130>
2023-11-30 06:22:31 +0100

    conflit

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -19,7 +19,9 @@ class AppExtension extends AbstractExtension
     public function customFunction(Request $request): string
     {
         $sessionArticles = $request->getSession()->get('panier');
-        $nbArticles = count($sessionArticles);
+
+        // Vérifiez si $sessionArticles n'est pas null avant d'appeler count
+        $nbArticles = is_array($sessionArticles) ? count($sessionArticles) : 0;
 
         // Logique de la fonction personnalisée
         return (string)$nbArticles;
