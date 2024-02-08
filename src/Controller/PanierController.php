6419cf847082bc9f4663115a9a34de428ec9ef43
jeremy Dubrulle <https://github.com/jeremy93130>
2024-02-08 18:01:32 +0100

    reparation panier

--- src/Controller/PanierController.php
+++ src/Controller/PanierController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Entity\Produits;
-use App\Repository\ProduitsRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
